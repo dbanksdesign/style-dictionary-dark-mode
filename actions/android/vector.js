dc818f41f027b198fa6fdd235a8af5a7123f0399
Danny Banks <djb@amazon.com>
2021-04-28 13:33:42 -0700

    Applying the multi-file method

--- actions/android/vector.js
+++ actions/android/vector.js
@@ -7,9 +7,18 @@ const s2v = require('svg2vectordrawable');
  * @param {String} options.svg - The content of the SVG that will be turned into a vector drawable. The SVG content at this point should have had all the token references inside of it resolved.
  * @param {String} options.name - The name of the image token
  * @param {String} options.androidPath - The build path for Android. This will be defined in the configuration
+ * @param {String} options.mode - The current mode (light or dark) Style Dictionary is being run in.
  */
-function androidVector({ androidPath, svg, name }) {
-  const outputPath = `${androidPath}drawable/${name}.xml`;
+function androidVector({ androidPath, svg, name, mode }) {
+  // Android doesn't support high contrast modes
+  if ([`hc`,`hcDark`].includes(mode)) {
+    return;
+  }
+  
+  const outputPath = mode === `dark` ?
+    `${androidPath}drawable-night/${name}.xml` :
+    `${androidPath}drawable/${name}.xml`;
+  
   fs.ensureFileSync(outputPath);
   
   // s2v will generate an Android vector drawable file
--- actions/ios/imagesets.js
+++ actions/ios/imagesets.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const sharp = require('sharp');
-const { contents, idiom } = require('./consts');
+const { contents, darkAppearance, idiom, hcAppearance } = require('./consts');
 
 /**
  * This function will generate an imageset for iOS
@@ -8,19 +8,42 @@ const { contents, idiom } = require('./consts');
  * @param {String} options.svg - The content of the SVG that will be turned into a PNG. The SVG content at this point should have had all the token references inside of it resolved.
  * @param {String} options.name - The name of the image token
  * @param {String} options.iosPath - The build path for iOS. This will be defined in the configuration
+ * @param {String} options.mode - The current mode (light or dark) Style Dictionary is building in.
  */
-function generateImageset({ svg, name, iosPath }) {
+function generateImageset({ svg, name, iosPath, mode }) {
   const outputPath = `${iosPath}StyleDictionary.xcassets/${name}.imageset`;
   fs.ensureDirSync(outputPath);
   
-  const filename = `img.png`;
-  const imageset = {
-    ...contents,
-    images: [{
-      idiom,
-      filename
-    }]
+  // The imageset might already exist because Style Dictionary is run multiple
+  // times with different configurations. If the imageset already exists we want
+  // to modify it rather than writing over it.
+  const imageset = fs.existsSync(`${outputPath}/Contents.json`) ?
+    fs.readJsonSync(`${outputPath}/Contents.json`) :
+    { ...contents, images: [] }
+  
+  let filename = `img.png`;
+  let image = {
+    idiom
+  };
+  
+  if (mode === `dark`) {
+    filename = `img-dark.png`;
+    image.appearances = [darkAppearance];
   }
+  
+  if (mode === `hc`) {
+    filename = `img-hc.png`;
+    image.appearances = [hcAppearance];
+  }
+  
+  if (mode === `hcDark`) {
+    filename = `img-hc-dark.png`;
+    image.appearances = [darkAppearance, hcAppearance];
+  }
+  
+  // Add the image to the images array of the imageset object.
+  image.filename = filename;
+  imageset.images.push(image);
 
   // Here we are using the sharp library for image processing that will take
   // the SVG content and render it as a PNG
